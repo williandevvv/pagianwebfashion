b4b638817dd1430cfdc860db137ebcdfb0f3b70f
MortySanchez <168171846+williandevvv@users.noreply.github.com>
2025-07-10 10:08:52 -0600

    feat: agrega modulo react

---
+++ src/react/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GaleriaProductos from './components/GaleriaProductos.jsx';
+import Carrito from './components/Carrito.jsx';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Fashion Collection React</h1>
+      <GaleriaProductos />
+      <Carrito />
+    </div>
+  );
+}
